3e633fef4c999859052c7638966547880d315e24
reinhardjs <reinhardjsilalahi@gmail.com>
2024-09-06 11:33:51 +0700

    feat(medusa-plugin-anamnesis): prepare migration generator configuration

---
+++ datasource.js
@@ -0,0 +1,41 @@
+const { DataSource } = require("typeorm")
+const dotenv = require("dotenv");
+
+let ENV_FILE_NAME = "";
+switch (process.env.NODE_ENV) {
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
+  default:
+    ENV_FILE_NAME = ".env";
+    break;
+}
+
+try {
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [
+    "dist/models/*.js",
+  ],
+  migrations: [
+    "dist/migrations/*.js",
+  ],
+})
+
+module.exports = {
+  datasource: AppDataSource,
+}
