23f298efd344f665d1dd8511fc9072a55c42bc38
reinhardjs <reinhardjsilalahi@gmail.com>
2024-09-11 21:27:37 +0700

    feat(medusa-plugin-anamnesis): added api route with express endpoint for getFormTemplateById

---
+++ src/api/index.ts
@@ -0,0 +1,13 @@
+import { Router } from "express"
+import { AnamnesisFormController } from "../controllers/anamnesis-form.controller";
+
+export default (rootDirectory, options) => {
+  const router = Router()
+  const anamnesisFormController = new AnamnesisFormController();
+
+  router.get("/admin/custom/forms/:id/template", (req, res) => {
+    anamnesisFormController.getFormTemplateById(req, res)
+  })
+
+  return router
+}
